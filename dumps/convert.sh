651b2d3df4793cdc1c9b982efc486a35072edb17
Zellyn Hunter <zellyn@squareup.com>
2018-05-20 20:26:02 -0400

    add lode runner

---
+++ dumps/convert.sh
@@ -0,0 +1,9 @@
+./hgr archon.hgr.bin ../images/archon.png
+./hgr archon-ii.hgr.bin ../images/archon-ii.png
+./hgr archon.hgr.bin ../images/archon.png
+./hgr bards-tale.hgr.bin ../images/bards-tale.png
+./hgr lode-runner.hgr.bin ../images/lode-runner.png
+./hgr championship-lode-runner.hgr.bin ../images/championship-lode-runner.png
+./hgr rescue-raiders-pic.hgr.bin ../images/rescue-raiders-pic.png
+./hgr rescue-raiders-words.hgr.bin ../images/rescue-raiders-words.png
+./hgr ancient-legends.hgr.bin ../images/ancient-legends.png
