59bf0a01adb9ad627e81324264fa03f29a77b466
Michaelangel007 <Michaelangel007@sharedcraft.com>
2018-05-24 08:27:30 -0600

    Cleanup

--- dumps/convert.sh
+++ dumps/convert.sh
@@ -1,10 +1,11 @@
-./hgr archon.hgr.bin ../images/archon.png
-./hgr archon-ii.hgr.bin ../images/archon-ii.png
-./hgr archon.hgr.bin ../images/archon.png
-./hgr bards-tale.hgr.bin ../images/bards-tale.png
-./hgr lode-runner.hgr.bin ../images/lode-runner.png
+./hgr ancient-legends.hgr.bin          ../images/ancient-legends.png
+./hgr archon-ii.hgr.bin                ../images/archon-ii.png
+./hgr archon.hgr.bin                   ../images/archon.png
+./hgr archon.hgr.bin                   ../images/archon.png
+./hgr bards-tale.hgr.bin               ../images/bards-tale.png
 ./hgr championship-lode-runner.hgr.bin ../images/championship-lode-runner.png
-./hgr rescue-raiders-pic.hgr.bin ../images/rescue-raiders-pic.png
-./hgr rescue-raiders-words.hgr.bin ../images/rescue-raiders-words.png
-./hgr ancient-legends.hgr.bin ../images/ancient-legends.png
-./hgr u5-shadowlords.hgr.bin ../images/u5-shadowlords.png
+./hgr lode-runner.hgr.bin              ../images/lode-runner.png
+./hgr rescue-raiders-pic.hgr.bin       ../images/rescue-raiders-pic.png
+./hgr rescue-raiders-words.hgr.bin     ../images/rescue-raiders-words.png
+./hgr u5-shadowlords.hgr.bin           ../images/u5-shadowlords.png
+
